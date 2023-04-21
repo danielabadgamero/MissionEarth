c8e72839fad78b577a3b370ad322a9d27bc672a4
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-04-21 20:59:53 +0200

    Add planet images.

--- HomeScreen.cpp
+++ HomeScreen.cpp
@@ -1,9 +1,21 @@
+#include <algorithm>
+
+#include <SDL_image.h>
+
 #include "Core.h"
 #include "HomeScreen.h"
 #include "Label.h"
 
 void HomeScreen::draw() const
 {
-	Label::setFont("C:\\Windows\\Fonts\\AGENCYR.TTF", 32);
-	Label::draw("Hello, world!", { Core::monitor.w / 2, Core::monitor.h / 2 }, { 0xff, 0xff, 0xff }, { 0.5, 0.5 });
+	SDL_Texture* texture{ IMG_LoadTexture(Core::renderer, "img/title.png") };
+	int w{};
+	int h{};
+	SDL_QueryTexture(texture, NULL, NULL, &w, &h);
+	SDL_FRect rect{ Core::monitor.w * 0.5f, 0 };
+	rect.w = std::clamp(static_cast<float>(w), Core::monitor.w * 0.1f, Core::monitor.w * 0.5f);
+	rect.h = std::clamp(static_cast<float>(h), Core::monitor.h * 0.1f, Core::monitor.h * 0.5f);
+	rect.x -= rect.w / 2;
+	SDL_RenderCopyF(Core::renderer, texture, NULL, &rect);
+	SDL_DestroyTexture(texture);
 }
