79bc12c15dec2f7c653616d678c8e4f290c7cfa4
Daniel Abad Gamero <danielabadgamero@gmail.com>
2023-04-21 19:21:54 +0200

    Remove Dear ImGui dependencies.

--- Core.cpp
+++ Core.cpp
@@ -1,9 +1,6 @@
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_net.h>
-#include <imgui.h>
-#include <backends/imgui_impl_sdl2.h>
-#include <backends/imgui_impl_sdlrenderer.h>
 
 #include "Core.h"
 
@@ -16,12 +13,6 @@ void Core::init(const char* title)
 	SDL_GetCurrentDisplayMode(0, &monitor);
 	window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, monitor.w, monitor.h, SDL_WINDOW_BORDERLESS | SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGui::GetIO();
-	ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
-	ImGui_ImplSDLRenderer_Init(renderer);
 
 	running = true;
 }
@@ -30,8 +21,6 @@ void Core::event()
 {
 	SDL_Event e;
 	while (SDL_PollEvent(&e))
-	{
-		ImGui_ImplSDL2_ProcessEvent(&e);
 		switch (e.type)
 		{
 		case SDL_QUIT:
@@ -46,28 +35,17 @@ void Core::event()
 			}
 			break;
 		}
-	}
 }
 
 void Core::draw()
 {
-	ImGui_ImplSDLRenderer_NewFrame();
-	ImGui_ImplSDL2_NewFrame();
-	ImGui::NewFrame();
-
-	ImGui::Render();
 	SDL_SetRenderDrawColor(renderer, 0x10, 0x10, 0x10, 0xFF);
 	SDL_RenderClear(renderer);
-	ImGui_ImplSDLRenderer_RenderDrawData(ImGui::GetDrawData());
 	SDL_RenderPresent(renderer);
 }
 
 void Core::quit()
 {
-	ImGui_ImplSDLRenderer_Shutdown();
-	ImGui_ImplSDL2_Shutdown();
-	ImGui::DestroyContext();
-
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
 
